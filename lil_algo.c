848c18a59d70276ca748376a43292dc18b2f067a
Tanguy BOURDEAU <tbourdea@e1r10p15.clusters.42paris.fr>
2023-01-25 08:59:01 +0100

    New norminetted files

--- lil_algo.c
+++ lil_algo.c
@@ -6,11 +6,73 @@
 /*   By: tbourdea <tbourdea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 14:50:53 by tbourdea          #+#    #+#             */
-/*   Updated: 2023/01/23 18:19:37 by tbourdea         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:29:25 by tbourdea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_push_up(t_stack **a, t_stack **b, t_stack *sorted, t_stack **cmd)
+{
+	if ((*a)->nb == ft_min(*a)
+		|| (ft_lstsize(sorted) >= 6 && (*a)->nb <= ft_median(sorted)))
+		ft_pb(a, b, cmd);
+	else
+	{
+		if (ft_next_move(*a) > 0)
+		{
+			while ((*a)->nb != ft_min(*a))
+			{
+				if (ft_lstsize(sorted) >= 6 && (*a)->nb <= ft_median(sorted))
+					break ;
+				ft_ra(a, cmd);
+			}
+		}
+		else
+		{
+			while ((*a)->nb != ft_min(*a))
+			{
+				if (ft_lstsize(sorted) >= 6 && (*a)->nb <= ft_median(sorted))
+					break ;
+				ft_rra(a, cmd);
+			}
+		}
+	}
+}
+
+void	ft_algo_three(t_stack **stack, t_stack **cmd)
+{
+	if (!*stack || !stack)
+		return ;
+	if (ft_max(*stack) == (*stack)->nb)
+		ft_ra(stack, cmd);
+	if (ft_min(*stack) == (*stack)->next->nb)
+		ft_sa(&(*stack)->nb, &(*stack)->next->nb, cmd);
+	if (ft_lstlast((*stack))->nb == ft_min(*stack))
+		ft_rra(stack, cmd);
+	if (!ft_check_order(*stack))
+	{
+		ft_rra(stack, cmd);
+		ft_sa(&(*stack)->nb, &(*stack)->next->nb, cmd);
+	}
+}
+
+void	ft_rev_algo_three(t_stack **stack, t_stack **cmd)
+{
+	if (!*stack || !stack)
+		return ;
+	if (ft_min(*stack) == (*stack)->nb)
+		ft_rb(stack, cmd);
+	if (ft_max(*stack) == (*stack)->next->nb)
+		ft_sb(&(*stack)->nb, &(*stack)->next->nb, cmd);
+	if (ft_lstlast((*stack))->nb == ft_max(*stack))
+		ft_rrb(stack, cmd);
+	if (!ft_check_rev_order(*stack))
+	{
+		ft_rrb(stack, cmd);
+		ft_sb(&(*stack)->nb, &(*stack)->next->nb, cmd);
+	}
+}
 
 void	ft_lil_algo(t_stack **a, t_stack **b, t_stack *sorted, t_stack **cmd)
 {
@@ -20,7 +82,7 @@ void	ft_lil_algo(t_stack **a, t_stack **b, t_stack *sorted, t_stack **cmd)
 			ft_sa(&(*a)->nb, &(*a)->next->nb, cmd);
 		else
 		{
-			if (ft_lstsize(*a) > 3)
+			while (ft_lstsize(*a) > 3)
 				ft_push_up(a, b, sorted, cmd);
 			if (ft_lstsize(*b) == 2 && !ft_check_rev_order(*b))
 				ft_sa(&(*b)->nb, &(*b)->next->nb, cmd);
--- list_manip.c
+++ list_manip.c
@@ -6,26 +6,11 @@
 /*   By: tbourdea <tbourdea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 18:15:52 by tbourdea          #+#    #+#             */
-/*   Updated: 2023/01/23 13:40:04 by tbourdea         ###   ########.fr       */
+/*   Updated: 2023/01/25 08:54:07 by tbourdea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-// void	ft_lstclear(t_stack **stack)
-// {
-// 	t_stack	*current;
-// 	t_stack	*tmp;
-
-// 	current = *stack;
-// 	while (current != NULL)
-// 	{
-// 		tmp = current->next;
-// 		free (current);
-// 		current = tmp;
-// 	}
-// }
-// *stack = NULL;
 
 int	ft_lst_cmp(t_stack *lst_a, t_stack *lst_b)
 {
@@ -63,7 +48,6 @@ t_stack	*list_inputs(int ac, char **av)
 	stack = ft_lstnew(ft_atol(av[1]));
 	i = 2;
 	new = NULL;
-	// ft_lstadd_back(&stack, new);
 	while (i < ac)
 	{
 		new = ft_lstnew(ft_atol(av[i]));
